c577e332d88906df1119cccaaebbbd6d7c09643a
Linnéa Toth <linnea@liroit.com>
2025-05-26 11:32:01 +0200

    Updates

--- src/Components/Footer.jsx
+++
@@ -1,7 +0,0 @@
-export default function Footer() {
-  return (
-    <footer className="bg-dark text-light text-center py-3 ">
-      {/* <p className="fst-italic ">2025 - REACT // something</p> */}
-    </footer>
-  );
-}
